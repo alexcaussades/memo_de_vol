2f08c1d3d49c81d7cf9f95470e941fb7536061c5
alexcaussades <alexcaussades@gmail.com>
2024-08-20 23:29:29 +0200

    VOR Callage

--- main.py
+++ main.py
@@ -10,16 +10,19 @@ import module.distance as distance
 # Load the configuration file
 with open("data/logiciel.json", "r") as file:
     logiciel = json.load(file)
+    
+with open("data/vor.json", "r") as files:
+    vor = json.load(files)
 
-print("Hello " + os.getlogin() + " welcome to the application " + logiciel["data"]["name"])
-time.sleep(1)
-print("system startup")
-time.sleep(1)
+# print("Hello " + os.getlogin() + " welcome to the application " + logiciel["data"]["name"])
+# time.sleep(1)
+# print("system startup")
+# time.sleep(1)
 
 print("What's your choice for this application? ")
 print("1. Calculate the descent rate")
 print("2. Calculate the estimated time")
-print("3. vor entry / next point")
+print("3. VOR entry / next point")
 print("7. Exit")
 
 choice = input("Enter your choice: ")
@@ -30,13 +33,9 @@ if choice == "1":
     vitesse_air = int(input("Enter the airspeed: "))
     distance_restante = int(input("Enter the remaining distance: "))
     vario_preffere = (input("Enter the preferred vario: "))
-    
-    try:
-        vario_preffere = int(vario_preffere)
-    except:
-        print("value for default 1500")
-        vario_preffere =  vario_preffere if vario_preffere != "" else 1500
-    
+    if vario_preffere == "":
+        print("The preferred vario is 1500")
+        vario_preffere = 1500
     
     d = distance.Distance(niveau_de_vol, vitesse_air, distance_restante, vario_preffere)
     print("The descent rate is: ", d.taux_de_descente())
@@ -47,5 +46,19 @@ if choice == "2":
     distance_restante = int(input("Enter the remaining distance: "))
     vario_preffere = int(input("Enter the preferred vario: "))
     
+    if vario_preffere == "":
+        print("The preferred vario is 1500")
+        vario_preffere = 1500
+    
     d = distance.Distance(niveau_de_vol, vitesse_air, distance_restante, vario_preffere)
     print("The estimated time is: ", d.estimation_temps())
+    
+if choice == "3":
+    choice = input("VOR next point: (0° and 360°) : ")
+    if choice:
+        print("The VOR entrer point is: ", vor["data"][choice])
+        
+        
+    
+
+    
