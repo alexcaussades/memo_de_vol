81eeb41a96201fb63dcf6fe6f6df3ee5bd4f655d
alexcaussades <alexcaussades@gmail.com>
2024-12-23 23:27:23 +0100

    j v2

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import logging
 import module.distance as distance
 import module.metar as metar
 
-# hello
+# hello v2
 # Load the configuration file
 with open("data/logiciel.json", "r") as file:
     logiciel = json.load(file)
